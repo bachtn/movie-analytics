891b78a560d6b6223ba47179d28f68f18d15f098
Alaa Bakhti <alaa.bakhti@epita.fr>
2017-07-05 01:26:54 +0200

    [MOVIE][NOT-TESTED] creates a dictionary object for each movie but didn't test it because of some problems with the api

--- src/prepare-data/imdbpy.py
+++ src/prepare-data/imdbpy.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python
 
 from imdb import IMDb
+from scraper import *
 
-# Create the object that will be used to access the IMDb's database.
-ia = IMDb()
 
-nbr_good_movies = 0
-#for movie_id in range(451279, 99999999):
-for movie_id in range(99):
-    try:
-        movie = ia.get_movie(str(movie_id))
-        print("title = " + movie['title'] + " " + movie['rating'])
-        ia.update(movie, 'release dates')
-        #print(movie['release dates'][0])
-        #print(movie['year'])
-        #print(movie['runtime']) #????
-        #print(movie['rating'])
-        #print(movie['votes'])
-        #print(movie['genres'])
-        #print(movie['country'])
-        print(ia.get_imdbURL(movie))
-        nbr_good_movies += 1
-        """
-        for person in ia.search_person('Mel Gibson'):
-            print(person.personID, person['name'])
-        """
-    except Exception as e:
-        #print(str(e))
-        pass
+def collect_movie_informations(id_list, by_id=True):
+    ia = IMDb()
+    nbr_good_movies = 0
+    #for movie_id in range(451279, 99999999):
+    for movie_id in id_list:
+        try:
+            movie = ia.get_movie(str(movie_id)) if by_id else ia.search_movie(movie_id)
+            title = movie['title']
+            ia.update(movie, 'release dates')
+            release_date = movie['release dates'][0]
+            year = movie['year']
+            #print(movie['runtime']) #????
+            rating = movie['rating']
+            nbr_votes = movie['votes']
+            genre = movie['genres']
+            country = movie['country']
+            movie_url = ia.get_imdbURL(movie)
+            reviews = get_movie_reviews(movie_url)
+            nbr_good_movies += 1
+            movie_object = { \
+                    'title': title, \
+                    'release_date': release_date, \
+                    'movie_year': year, \
+                    'rating': rating, \
+                    'nbr_votes': nbr_votes, \
+                    'genre': genre,  \
+                    'country': country, \
+                    'movie_url': movie_url, \
+                    'reviews': reviews \
+                    }
+            """
+            for person in ia.search_person('Mel Gibson'):
+                print(person.personID, person['name'])
+            """
+        except Exception as e:
+            #print(str(e))
+            pass
 
-print(nbr_good_movies)
+collect_movie_informations(range(99))
--- src/prepare-data/scraper.py
+++ src/prepare-data/scraper.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-#div class reviews > div id tn15main > div id tn15content > 
-
 import requests
 from bs4 import BeautifulSoup as bs
-
+import json
 
 def get_review_page_urls(movie_url):
+    """
+    Given a movie_url, it returns a list of urls of all review pages.
+    In fact, if a movie has more than 10 reviews, each 10 reviews are in a different page.
+    """
     review_url = movie_url + '/reviews'
     soup = bs(requests.get(review_url).content, "html.parser")
     review_page_urls = []
@@ -16,12 +18,21 @@ def get_review_page_urls(movie_url):
     return set(review_page_urls)
 
 def review_filter(tag):
+    """
+    Determines if the given tag is a tag of a review div.
+    The review div filter has no class and no id, and has a tag h2.
+    """
     return not(tag.has_attr('class')) and not(tag.has_attr('id')) and tag.find("h2")
 
 def get_reviews_in_page(review_page_url):
+    """
+    Given an url of a review page, it iterates over all reviews
+    and collects the data of each review, then returns a list
+    of a dictionary object that represents a review
+    """
     reviews = []
     soup = bs(requests.get(review_page_url).content, "html.parser")
-    for review_container in soup.find_all('div', class_=False, id_=False):
+    for review_container in soup.find_all('div'):
         if (review_filter(review_container)):
             author_id = review_container.find("a").get("href").split('/')[2]
             title = review_container.find("h2").get_text()
@@ -32,6 +43,11 @@ def get_reviews_in_page(review_page_url):
     return reviews
 
 def get_movie_reviews(movie_url):
+    """
+    Gets the url of all review pages and than, for each page,
+    it gets its reviews.
+    Returns a list of all the reviews of the given movie.
+    """
     review_page_urls = get_review_page_urls(movie_url)
     movie_reviews = []
     for review_page_url in review_page_urls:
@@ -40,7 +56,13 @@ def get_movie_reviews(movie_url):
     return movie_reviews
         
 
-review_url = 'http://akas.imdb.com/title/tt0000001/reviews?start=0'
 
+"""
 movie_url = 'http://akas.imdb.com/title/tt0000001'
-print(len(get_movie_reviews(movie_url)))
+review_list = get_movie_reviews(movie_url)
+print("number of reviews = " + str(len(review_list)))
+reviews_in_json = json.dumps(review_list)
+with open('data.json', 'w') as outfile:
+    json.dump(reviews_in_json, outfile)
+"""
+
