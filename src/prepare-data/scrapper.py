a569804b2f24f8642c9f1e829de57040010d1131
Alaa Bakhti <alaa.bakhti@epita.fr>
2017-07-04 23:04:55 +0200

    [REVIEWS] get all reviews given a movie url and creates a list of dictionary object representing the list of reviews

--- src/prepare-data/scrapper.py
+++ src/prepare-data/scrapper.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 
+#div class reviews > div id tn15main > div id tn15content > 
+
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'http://akas.imdb.com/title/tt0000001' + '/reviews'
-r = requests.get(url)
-soup = bs(r.content, "html.parser")
 
+def get_review_page_urls(movie_url):
+    review_url = movie_url + '/reviews'
+    soup = bs(requests.get(review_url).content, "html.parser")
+    review_page_urls = []
+    review_page_container = soup.find('div', {"id" : "tn15content"}).find_all('table')[1]
+    for page_url in review_page_container.find_all("a"):
+        review_page_urls.append(movie_url + '/' + page_url.get("href"))
+    return set(review_page_urls)
 
 def review_filter(tag):
-    return not(tag.has_attr('class')) and not(tag.has_attr('id')) \
-            and tag.find("h2")
+    return not(tag.has_attr('class')) and not(tag.has_attr('id')) and tag.find("h2")
 
-for review_container in soup.find_all('div', class_=False, id_=False):
-    if (review_filter(review_container)):
-        author_id = review_container.find("a").get("href").split('/')[2]
-        title = review_container.find("h2").get_text()
-        review_date = review_container.find_all("small")[-1].get_text()
-        content = review_container.find_next_sibling().get_text()
-        print(author_id)
-        print(review_date)
-        print(title)
-        print(content)
+def get_reviews_in_page(review_page_url):
+    reviews = []
+    soup = bs(requests.get(review_page_url).content, "html.parser")
+    for review_container in soup.find_all('div', class_=False, id_=False):
+        if (review_filter(review_container)):
+            author_id = review_container.find("a").get("href").split('/')[2]
+            title = review_container.find("h2").get_text()
+            review_date = review_container.find_all("small")[-1].get_text()
+            content = review_container.find_next_sibling().get_text()
+            review = {'author_id': author_id, 'title': title, 'review_date': review_date, 'content': content}
+            reviews.append(review)
+    return reviews
+
+def get_movie_reviews(movie_url):
+    review_page_urls = get_review_page_urls(movie_url)
+    movie_reviews = []
+    for review_page_url in review_page_urls:
+        print(review_page_url)
+        movie_reviews.extend(get_reviews_in_page(review_page_url))
+    return movie_reviews
+        
+
+review_url = 'http://akas.imdb.com/title/tt0000001/reviews?start=0'
+
+movie_url = 'http://akas.imdb.com/title/tt0000001'
+print(len(get_movie_reviews(movie_url)))
